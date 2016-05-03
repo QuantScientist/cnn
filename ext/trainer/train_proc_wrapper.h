7874249c2bf43e82ea234748ea18a102f1e95027
kaishengyao <kaishengy@gmail.com>
2016-05-03 14:02:25 -0700

    add a function to do MERT idf weight tuning

--- ext/trainer/train_proc_wrapper.h
+++ ext/trainer/train_proc_wrapper.h
@@ -333,26 +333,39 @@ int main_body(variables_map vm, size_t nreplicate = 0, size_t decoder_additiona_
                 largest_cost, reward_baseline, threshold_prob, vm["do_gradient_check"].as<bool>());
         }
     }
-    else if (vm["epochsize"].as<int>() >1 && !vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus"))
+    else if (vm["epochsize"].as<int>() >1 && !vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus") && vm.count("train") > 0)
     {   // split data into nparts and train
         training.clear();
         ptrTrainer->split_data_batch_train(vm["train"].as<string>(), model, hred, devel, *sgd, fname, vm["epochs"].as<int>(), vm["nparallel"].as<int>(), vm["epochsize"].as<int>(), vm["segmental_training"].as<bool>(), vm["do_gradient_check"].as<bool>(), vm["padding"].as<bool>());
     }
-    else if (vm.count("nparallel") && !vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus"))
+    else if (vm.count("nparallel") && !vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus") && training.size() > 0)
     {
         ptrTrainer->batch_train(model, hred, training, devel, *sgd, fname, vm["epochs"].as<int>(), vm["nparallel"].as<int>(), largest_dev_cost, vm["segmental_training"].as<bool>(), true, vm["do_gradient_check"].as<bool>(), true, vm["padding"].as<bool>(), kSRC_EOS);
     }
-    else if (!vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus"))
+    else if (!vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus") && training.size() > 0)
     {
         ptrTrainer->train(model, hred, training, devel, *sgd, fname, vm["epochs"].as<int>(), vm.count("charlevel") > 0, vm.count("nosplitdialogue"));
     }
-    else if (vm.count("testcorpus"))
+    else if (vm.count("tunereranking") && devel.size() > 0 && vm.count("getidf"))
+    {
+        if (vm.count("outputfile") == 0)
+        {
+            throw std::invalid_argument("missing recognition output file");
+        }
+        ptrTrainer->MERT_tune(model, hred, devel, vm["outputfile"].as<string>(), sd);
+    }
+
+    else if (vm.count("testcorpus") && testcorpus.size() > 0)
     {
         if (vm.count("outputfile") == 0)
         {
             throw std::invalid_argument("missing recognition output file");
         }
         ptrTrainer->test(model, hred, testcorpus, vm["outputfile"].as<string>(), sd, test_numturn2did, vm["segmental_training"].as<bool>());
+    }
+    else
+    {
+        cerr << "cannot find an operation to the corresponding arguments to run" << endl;
     }
 
     delete sgd;
