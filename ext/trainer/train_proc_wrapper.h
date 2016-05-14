1e767c2141c0af5c5a441cb44950524c063ef42d
kaishengyao <kaishengy@gmail.com>
2016-05-13 20:35:56 -0700

    add functions to use additional features for training and test

--- ext/trainer/train_proc_wrapper.h
+++ ext/trainer/train_proc_wrapper.h
@@ -177,7 +177,7 @@ int main_body(variables_map vm, size_t nreplicate = 0, size_t decoder_additiona_
             throw std::invalid_argument("missing --outputfile");
         }
 
-        if (vm.count("ranker"))
+        if (vm["ranker"].as<bool>())
         {
             cerr << "Reading training corpus from " << vm["train"].as<string>() << "...\n";
             training = read_corpus(vm["train"].as<string>(), sd, kSRC_SOS, kSRC_EOS, vm["mbsize"].as<int>(), true);
@@ -339,11 +339,11 @@ int main_body(variables_map vm, size_t nreplicate = 0, size_t decoder_additiona_
     else if (vm["epochsize"].as<int>() >1 && !vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus") && vm.count("train") > 0)
     {   // split data into nparts and train
         training.clear();
-        ptrTrainer->split_data_batch_train(vm["train"].as<string>(), model, hred, devel, *sgd, fname, vm["epochs"].as<int>(), vm["nparallel"].as<int>(), vm["epochsize"].as<int>(), vm["segmental_training"].as<bool>(), vm["do_gradient_check"].as<bool>(), vm["padding"].as<bool>());
+        ptrTrainer->split_data_batch_train(vm["train"].as<string>(), model, hred, devel, *sgd, fname, vm["epochs"].as<int>(), vm["nparallel"].as<int>(), vm["epochsize"].as<int>(), vm["segmental_training"].as<bool>(), vm["do_gradient_check"].as<bool>(), vm["padding"].as<bool>(), vm["withadditionalfeature"].as<bool>());
     }
     else if (vm.count("nparallel") && !vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus") && training.size() > 0)
     {
-        ptrTrainer->batch_train(model, hred, training, devel, *sgd, fname, vm["epochs"].as<int>(), vm["nparallel"].as<int>(), largest_dev_cost, vm["segmental_training"].as<bool>(), true, vm["do_gradient_check"].as<bool>(), true, vm["padding"].as<bool>(), kSRC_EOS);
+        ptrTrainer->batch_train(model, hred, training, devel, *sgd, fname, vm["epochs"].as<int>(), vm["nparallel"].as<int>(), largest_dev_cost, vm["segmental_training"].as<bool>(), true, vm["do_gradient_check"].as<bool>(), true, vm["padding"].as<bool>(), kSRC_EOS, vm["withadditionalfeature"].as<bool>());
     }
     else if (!vm.count("test") && !vm.count("kbest") && !vm.count("testcorpus") && training.size() > 0)
     {
@@ -360,16 +360,18 @@ int main_body(variables_map vm, size_t nreplicate = 0, size_t decoder_additiona_
         else
             ptrTrainer->MERT_tune_edit_distance(model, hred, devel, vm["outputfile"].as<string>(), sd, weight_IDF);
     }
-
-    else if (vm.count("testcorpus") && testcorpus.size() > 0 && !vm.count("ranker"))
+    else if (vm.count("testcorpus") && testcorpus.size() > 0 && !vm["ranker"].as<bool>())
     {
         if (vm.count("outputfile") == 0)
         {
             throw std::invalid_argument("missing recognition output file");
         }
-        ptrTrainer->test(model, hred, testcorpus, vm["outputfile"].as<string>(), sd, test_numturn2did, vm["segmental_training"].as<bool>());
+        if (vm["withadditionalfeature"].as<bool>())
+            ptrTrainer->test_with_additional_feature(model, hred, testcorpus, vm["outputfile"].as<string>(), sd);
+        else
+            ptrTrainer->test(model, hred, testcorpus, vm["outputfile"].as<string>(), sd, test_numturn2did, vm["segmental_training"].as<bool>());
     }
-    else if (vm.count("ranker") && testcorpus.size() > 0 && training.size() > 0)
+    else if (vm["ranker"].as<bool>() && testcorpus.size() > 0 && training.size() > 0)
     {
         ptrTrainer->testRanking(model, hred, testcorpus, training, vm["outputfile"].as<string>(), sd, test_numturn2did, vm["ranking_using_tfidf"].as<bool>());
     }
